7020c36640ec567f0ddb9f38e05081d455cbba2a
Meredith Reece <mardebugger@gmail.com>
2018-08-02 23:41:16 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,8 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(user_input)
-    @students = self.find_by(name: user_input)
+    if !user_input.empty?
+      where('NAME like ?', "%#{user_input}%")
+    end
   end
 end
