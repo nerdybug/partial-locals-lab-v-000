f9d438de983dbfde85f1e2ea57af2f0fd76eeb74
Meredith Reece <mardebugger@gmail.com>
2018-08-02 23:37:28 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(user_input)
+    self.find_by(name: user_input)
+  end
 end
