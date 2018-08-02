8da80dfb46d07c20a2c38f477856cafcdffb149c
Meredith Reece <mardebugger@gmail.com>
2018-08-02 23:35:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.find_by(name: params[:user_input])
   end
 
   def student_params
